aee8a65bb39ee99ef9213527b68324f5228954d6
Nyi Min Khant <nyiminkx01@gmail.com>
2025-12-21 22:51:15 -0800

    Patch and delete added

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Post } from "../models/post.model.js";
 
 const createPost = async (req, res) => {
@@ -65,4 +66,75 @@ const getPostByUser = async (req, res) => {
     }
 }
 
-export {createPost, getPosts, getPostByUser}
+const updatePost = async (req, res) => {
+    try {
+
+        const id  = req.params.id;
+
+        //validating id coming from request is valid or not!
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ 
+                message: "Invalid Record ID" 
+            });
+        }
+
+        if(Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                message: "No data provided."
+            });
+        }
+
+        const post = await Post.findByIdAndUpdate(
+            id,         //mongoose automatically converts string to ObjectId
+            req.body,
+            {new: true}
+        );
+
+        if(!post) return res.status(404).json({
+            message: "Post not found."
+        });
+
+        return res.status(200).json({
+            message: "A post is updated successfully.",
+            post
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error: error.message
+        })
+    } 
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                message: "Invalid Record ID."
+            });
+        }
+
+        const post = await Post.findByIdAndDelete(id);
+
+        if(!post) return res.status(404).json({
+            message: "Post Not Found."
+        });
+
+        return res.status(200).json({
+            message: "A post is deleted successfully.",
+            post
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error: error.message
+        })
+    } 
+}
+
+export {createPost, getPosts, getPostByUser, updatePost, deletePost}
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { createPost, getPosts, getPostByUser } from "../controllers/post.controller.js";
+import { createPost, getPosts, getPostByUser, updatePost, deletePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.post("/create", createPost);
+
 router.get("", getPosts);
 router.get("/user/:userId", getPostByUser);
+
+router.patch("/update/:id", updatePost);
+
+router.delete("/delete/:id", deletePost);
 
 export default router;
